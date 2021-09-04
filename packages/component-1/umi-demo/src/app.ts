0f52ba4995af5bff4aba25c9cbf34e48069dde93
LeoKu <czc12580520@gmail.com>
2021-09-04 14:39:11 +0800

    update

--- packages/component-1/umi-demo/src/app.ts
+++
@@ -1,9 +0,0 @@
-import Demo from '../../src/components/Demo'
-
-export function patchRoutes({ routes }: { routes: any[] }) {
-  console.log(routes)
-  routes.unshift({
-    path: '/demo',
-    component: Demo,
-  })
-}
